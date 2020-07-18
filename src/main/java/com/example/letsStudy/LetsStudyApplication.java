443a46a72752faadb76db06af9c9ea6eea2bfd97
Jiyoung <wnsekd2333@gmail.com>
2020-07-18 18:33:40 +0900

    Initial commit

---
+++ src/main/java/com/example/letsStudy/LetsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.letsStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/letsStudy/LetsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.letsStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
